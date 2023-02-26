523d960cad3dbf53e4fc3886307a7e1a616861e4
ivrivr237 <87616498+ivrivr237@users.noreply.github.com>
2023-02-26 23:01:08 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,72 +1,91 @@
+import os
 import logging
 import subprocess
-import telebot
-import time
-import re
-import os
-from telebot.types import Message
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
 logger = logging.getLogger(__name__)
 
-bot = telebot.TeleBot('6239760029:AAFBtIRSmuS-N6KtVrKourikxV7J_39nn8U')
+# Define the output preset settings
+PRESETS = {
+    'Normal': ['-e', 'x264', '-q', '22'],
+    'High Quality': ['-e', 'x264', '-q', '18', '--optimize'],
+    'Very High Quality': ['-e', 'x265', '-q', '22', '--x265-preset', 'slow', '--x265-profile', 'main10', '--optimize'],
+    'Fast 480p': ['-e', 'x264', '-q', '20', '--optimize', '--preset', 'fast', '--width', '854', '--height', '480'],
+    'Fast 720p': ['-e', 'x264', '-q', '20', '--optimize', '--preset', 'fast', '--width', '1280', '--height', '720'],
+}
 
-@bot.message_handler(commands=['start'])
-def handle_start(message: Message) -> None:
-    bot.reply_to(message, 'Hi! Send me a video file and I will transcode it using HandBrake CLI.')
+def start(update, context):
+    update.message.reply_text('Hi! Send me a video and I will transcode it using HandBrakeCLI.')
 
-@bot.message_handler(content_types=['video'])
-def handle_video(message: Message) -> None:
-    # Create input and output folders if they don't exist
+def choose_preset(update, context):
+    keyboard = []
+    for preset_name in PRESETS:
+        keyboard.append([InlineKeyboardButton(preset_name, callback_data=preset_name)])
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text('Please choose an output preset:', reply_markup=reply_markup)
+
+def video_handler(update, context):
+    video_file = context.bot.getFile(update.message.video.file_id, timeout=100)
+    video_file_name = os.path.basename(video_file.file_path)
+
     if not os.path.exists('input'):
-        os.mkdir('input')
+        os.makedirs('input')
+
     if not os.path.exists('output'):
-        os.mkdir('output')
-    
-    # Download the video file
-    file_info = bot.get_file(message.video.file_id)
-    downloaded_file = bot.download_file(file_info.file_path)
-    
-    # Transcode the video using HandBrake CLI
-    with open('input/input.mp4', 'wb') as f:
-        f.write(downloaded_file)
-    command = ['HandBrakeCLI', '-i', 'input/input.mp4', '-o', 'output/output.mp4']
-    p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
-    
-    # Send status messages to the user every 3 seconds while transcoding
-    while p.poll() is None:
-        output = p.stdout.readline()
-        if not output:
-            break
-        # Extract progress information from HandBrake CLI output
-        percent_match = re.search(r'\b(\d+(?:\.\d+)?)%\b', output)
-        fps_match = re.search(r'\b(\d+(?:\.\d+)?) fps\b', output)
-        eta_match = re.search(r'\bETA (\d+):(\d+):(\d+)\b', output)
-        if percent_match and fps_match and eta_match:
-            percent = percent_match.group(1)
-            fps = fps_match.group(1)
-            eta_hours = int(eta_match.group(1))
-            eta_minutes = int(eta_match.group(2))
-            eta_seconds = int(eta_match.group(3))
-            eta = f'{eta_hours:02d}:{eta_minutes:02d}:{eta_seconds:02d}'
-            message_text = f'Transcoding video... {percent}% complete, {fps} fps, ETA {eta}'
-            bot.send_message(message.chat.id, message_text)
-        time.sleep(3)
-    
-    # Delete the input video file
-    os.remove('input/input.mp4')
-    
-    # Check if transcoding process had an error
-    if p.returncode != 0:
-        error_message = 'An error occurred while transcoding the video. Please try again.'
-        bot.send_message(message.chat.id, error_message)
+        os.makedirs('output')
+
+    input_file_path = os.path.join('input', video_file_name)
+    video_file.download(input_file_path)
+
+    # Check the size of the video file
+    video_file_size = os.path.getsize(input_file_path)
+    if video_file_size > 2 * 1024 * 1024 * 1024:  # 2 GB in bytes
+        update.message.reply_text('The video file is too large and cannot be sent via Telegram.')
+        return
+
+    # Get the output preset settings from the callback data (if available) or use the 'Normal' preset as the default
+    if 'preset' in context.chat_data:
+        preset_name = context.chat_data['preset']
     else:
-        # Send the transcoded video back to the user
-        with open('output/output.mp4', 'rb') as f:
-            bot.send_video(message.chat.id, f)
-        
-        # Delete the output video file
-        os.remove('output/output.mp4')
-        
-bot.polling()
+        preset_name = 'Normal'
+
+    preset_settings = PRESETS[preset_name]
+
+    output_file_path = os.path.join('output', f'{os.path.splitext(video_file_name)[0]}.mkv')
+    subprocess.run(['HandBrakeCLI', '-i', input_file_path, '-o', output_file_path] + preset_settings)
+
+    context.bot.send_video(chat_id=update.effective_chat.id, video=open(output_file_path, 'rb'), supports_streaming=True)
+
+def button_handler(update, context):
+    query = update.callback_query
+    query.answer()
+
+    # Set the output preset in the chat data
+    context.chat_data['preset'] = query.data
+
+    # Run the video handler to transcode the video using the new preset
+    video_handler(update, context)
+
+def error(update, context):
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+TOKEN = '6239760029:AAFBtIRSmuS-N6KtVrKourikxV7J_39nn8U'
+
+updater = Updater(TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('preset', choose_preset))
+dispatcher.add_handler(MessageHandler(Filters.video, video_handler))
+# Add button handler to respond to user's preset selection
+dispatcher.add_handler(CallbackQueryHandler(button_handler))
+
+# Add error handler to log any errors
+dispatcher.add_error_handler(error)
+
+# Start the bot
+updater.start_polling()
+updater.idle()
 
